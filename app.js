d75b94b6f2666df4de5b0e463c415f2884fdbf76
SEKTION <SUKHBIRBRAR5225@GMAIL.COM>
2023-03-08 18:24:13 -0500

    second commit

--- app.js
+++ app.js
@@ -29,6 +29,7 @@ var text = document.querySelector("textarea");
   text.addEventListener(event, function () {
     var pgraph = document.querySelector("#word-count");
     var words = text.value;
+	var characters = words.replaceAll('\n','');
     var lines = words.split("\n");
     lines = lines.length;
     for (let i = 0; i < chars.length; i++) {
@@ -39,6 +40,6 @@ var text = document.querySelector("textarea");
     for (var i = 0; i < words.length; i++) {
       (words[i] != "" && words[i] != ";" && words[i] != "." && words[i] != "," && words[i] != ";") ? count +=1 : count;
     }
-    pgraph.innerHTML =`Words:${count} Characters:${text.value.length} Lines:${lines}`;
+    pgraph.innerHTML =`Words:${count} Characters:${characters.length} Lines:${lines}`;
   })
 );
